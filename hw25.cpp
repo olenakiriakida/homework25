7534ea8fb80e690e0ed2ccffadad605a02bdbaea
bob dylan kinnie <135342698+olenakiriakida@users.noreply.github.com>
2023-12-22 03:26:08 +0200

    Add files via upload

---
+++ hw25.cpp
@@ -0,0 +1,129 @@
+﻿#include <iostream>
+using namespace std;
+
+class MusicalAbility 
+{
+public:
+    virtual void UseMusicalAbility() const = 0;
+};
+
+class GuitarAbility : public MusicalAbility
+{
+public:
+    void UseMusicalAbility() const
+    {
+        cout << "Plays guitar\n";
+    }
+};
+
+class BassAbility : public MusicalAbility
+{
+public:
+    void UseMusicalAbility() const
+    {
+        cout << "Plays bass\n";
+    }
+};
+
+class DrumAbility : public MusicalAbility
+{
+public:
+    void UseMusicalAbility() const
+    {
+        cout << "Plays drums\n";
+    }
+};
+
+class Band
+{
+public:
+    virtual void PrintBand() const = 0;
+};
+
+class ConflictType
+{
+public:
+    virtual void PrintConflictType() const = 0;
+};
+
+class Peaceful : public ConflictType
+{
+public:
+    void PrintConflictType() const
+    {
+        cout << "Peaceful\n";
+    }
+};
+
+class Conflict : public ConflictType
+{
+public:
+    void PrintConflictType() const
+    {
+        cout << "Conflict\n";
+    }
+};
+
+class EricClapton : public Band, public GuitarAbility, public Peaceful
+{
+public:
+    void PrintBand() const
+    {
+        cout << "Eric Clapton\n";
+    }
+
+    void UseAbility() const
+    {
+        UseMusicalAbility();
+    }
+};
+
+class JackBruce : public Band, public BassAbility, public Conflict
+{
+public:
+    void PrintBand() const
+    {
+        cout << "Jack Bruce\n";
+    }
+
+    void UseAbility() const
+    {
+        UseMusicalAbility();
+    }
+};
+
+class GingerBaker : public Band, public DrumAbility, public Conflict
+{
+public:
+    void PrintBand() const
+    {
+        cout << "Ginger Baker\n";
+    }
+
+    void UseAbility() const
+    {
+        UseMusicalAbility();
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    EricClapton eric;
+    eric.PrintBand();
+    eric.PrintConflictType();
+    eric.UseAbility();
+    cout << "\n";
+
+    JackBruce jack;
+    jack.PrintBand();
+    jack.PrintConflictType();
+    jack.UseAbility();
+    cout << "\n";
+
+    GingerBaker ginger;
+    ginger.PrintBand();
+    ginger.PrintConflictType();
+    ginger.UseAbility();
+}
